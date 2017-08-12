e35adb6c4868b883f72a6e79763495a1913db2b5
Kristina <isitiriss@gmail.com>
2017-08-12 22:36:37 +0300

    Added telegram bot implementation.

--- main.go
+++ main.go
@@ -6,10 +6,13 @@ import (
 	"io/ioutil"
 	"net/http"
 	"strings"
+	"sync"
+	"time"
 
 	_ "github.com/KristinaEtc/slflog"
 
 	"github.com/KristinaEtc/config"
+	"github.com/Syfaro/telegram-bot-api"
 	"github.com/ventu-io/slf"
 )
 
@@ -34,22 +37,35 @@ type JiraConf struct {
 	ProjectName string
 }
 
+// TelegramConf is a file with telegram stuff.
+type TelegramConf struct {
+	Token   string
+	Timeout int
+	Debug   bool
+}
+
 // ConfFile is a file with all program options
 type ConfFile struct {
-	Name     string
-	Choosing string
-	Jira     JiraConf
+	Name       string
+	Nomination string
+	Jira       JiraConf
+	Telegram   TelegramConf
 }
 
 var globalConf = ConfFile{
-	Name:     "of-the-day program",
-	Choosing: "The fool",
+	Name:       "of-the-day program",
+	Nomination: "The fool",
 	Jira: JiraConf{
 		Address:     "localhost",
 		User:        "guest",
 		Password:    "guest",
 		Path:        "/rest/api/2/user/assignable/search?project=",
 		ProjectName: "MyProject",
+	},
+	Telegram: TelegramConf{
+		Token:   "token",
+		Timeout: 60,
+		Debug:   true,
 	},
 }
 
@@ -59,17 +75,36 @@ type colleague struct {
 	DisplayName string `json:"displayName"`
 }
 
-func choose(scopeTable map[string]int) string {
-	// TODO: choose graceful with metrics considering
+var winnerUser winner
+
+type winner struct {
+	nomitation string
+	winner     string
+	updateDay  int
+	m          *sync.Mutex
+}
+
+func getRandomColleague(scopeTable map[string]int) string {
 	for k := range scopeTable {
 		return k
 	}
 	return ""
 }
 
+func updateWinner(scopeTable map[string]int) {
+	// TODO: choose graceful with metrics considering
+	winnerUser.m.Lock()
+	defer winnerUser.m.Unlock()
+
+	_, _, dayNow := time.Now().Date()
+	if winnerUser.updateDay == dayNow {
+		return
+	}
+	winnerUser.winner = getRandomColleague(scopeTable)
+}
+
 func initScopeTable(colleagues []colleague) (scopeTable map[string]int) {
 	scopeTable = make(map[string]int)
-	log.Infof("%+v", colleagues)
 	for _, c := range colleagues {
 		scopeTable[c.DisplayName] = 0
 	}
@@ -107,9 +142,52 @@ func getColleagues(conf JiraConf) (responceBody, error) {
 	if err := decoder.Decode(&pData); err != nil {
 		return nil, fmt.Errorf("decoding responce body: %s", err.Error())
 	}
-	//}err == io.EOF {
-
 	return pData, nil
+}
+
+func run(conf TelegramConf, scopeTable map[string]int) (err error) {
+	bot, err := tgbotapi.NewBotAPI(conf.Token)
+	if err != nil {
+		return fmt.Errorf("connecting to a bot: %s", err.Error())
+	}
+
+	if conf.Debug {
+		bot.Debug = true
+	}
+	log.Infof("Authorized on account %s", bot.Self.UserName)
+	log.Debugf("Token=[%s]", conf.Token)
+
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = conf.Timeout
+
+	updates, err := bot.GetUpdatesChan(u)
+	if err != nil {
+		return fmt.Errorf("getting update chan: %s", err.Error())
+	}
+
+	var responseMsg string
+	var msg tgbotapi.MessageConfig
+
+	for update := range updates {
+		if update.Message == nil {
+			continue
+		}
+		log.Debugf("[%s] %s", update.Message.From.UserName, update.Message.Text)
+		switch update.Message.Text {
+		case "lox":
+			log.Debug("lox detected")
+			updateWinner(scopeTable)
+			responseMsg = fmt.Sprintf("%s of the day is... %s. GRATS!", globalConf.Nomination, winnerUser.winner)
+			msg = tgbotapi.NewMessage(update.Message.Chat.ID, responseMsg)
+			msg.ReplyToMessageID = update.Message.MessageID
+		default:
+			responseMsg = "Slabo stelish, pirozhok. Poprobuj \"lox\" command"
+			msg = tgbotapi.NewMessage(update.Message.Chat.ID, responseMsg)
+			msg.ReplyToMessageID = update.Message.MessageID
+		}
+		bot.Send(msg)
+	}
+	return nil
 }
 
 func main() {
@@ -117,11 +195,11 @@ func main() {
 	log.Infof("%s", globalConf.Name)
 	log.Error("----------------------------------------------")
 
-	log.Infof("BuildDate=%s\n", BuildDate)
-	log.Infof("GitCommit=%s\n", GitCommit)
-	log.Infof("GitBranch=%s\n", GitBranch)
-	log.Infof("GitState=%s\n", GitState)
-	log.Infof("GitSummary=%s\n", GitSummary)
+	log.Infof("BuildDate=%s", BuildDate)
+	log.Infof("GitCommit=%s", GitCommit)
+	log.Infof("GitBranch=%s", GitBranch)
+	log.Infof("GitState=%s", GitState)
+	log.Infof("GitSummary=%s", GitSummary)
 	log.Infof("VERSION=%s\n", Version)
 
 	colleagues, err := getColleagues(globalConf.Jira)
@@ -133,7 +211,16 @@ func main() {
 	scopeTable := initScopeTable(colleagues)
 	log.Debugf("Scope table: %+v", scopeTable)
 
-	winner := choose(scopeTable)
-	log.Infof("%s of the day is... %s. GRATS!", globalConf.Choosing, winner)
+	_, _, currDay := time.Now().Date()
+	winnerUser = winner{
+		nomitation: globalConf.Nomination,
+		updateDay:  currDay,
+		m:          &sync.Mutex{},
+		winner:     getRandomColleague(scopeTable),
+	}
 
+	err = run(globalConf.Telegram, scopeTable)
+	if err != nil {
+		log.Errorf("running: %s", err.Error())
+	}
 }
